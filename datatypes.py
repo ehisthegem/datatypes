2b2a5ea4a6885748dad6a7ab9b860378db6d13d1
Israel Ozioma Idahosa <israelidahosa24@gmail.com>
2024-09-08 22:46:19 +0100

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,47 @@
+#basics of python(variables and values)
+'''senwin = ['analyst', 'cybersecurity']
+print(senwin)
+ehis = ['male', 'tall']
+
+print(ehis)'''
+
+football = ['barcelona', 'manchester city', 'liverpool', 'arsenal']
+print(football)
+
+maths_score = [35, 50, 'eighty_nine']
+print(maths_score)
+
+economics = [25.7, 'seventy', 86, 'twenty_nine', 79.12, 90]
+print(economics)
+
+food, snacks, takeouts = ['burgers', 'salads', 'french fries']
+print(food)
+print(snacks)
+print(takeouts)
+
+basketball = volleyball = track_event = ['olympic']
+print(basketball)
+print(volleyball)
+print(track_event)
+
+basketball = volleyball = ['players']
+print(basketball)
+print(volleyball)
+
+food, snacks = ['wagyu', 'alfredo spag']
+print(food)
+print(snacks)
+
+football = ('messi', 10)
+print( type (football) )
+
+stores = {'jendol', 'twins faja', 'shoprite'}
+print( type (stores) )
+
+
+food = ['wagyu', 'alfredo spag']
+print( type (food) )
+
+stores_2 = set (('justrite', 'bokku mart', 'grocery bazzar'))
+print( type (stores_2) )
+
